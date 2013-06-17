520b1081be733d3f86be7d560a7a6a57ff512341
LuckyCoinAdministrator <administrator@luckycoinfoundation.org>
2013-06-17 22:12:53 +0300

    Update checkpoints.cpp
    Security update: Added more checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -27,10 +27,15 @@ namespace Checkpoints
 	// no checkpoint now, can be added in later releases
     static MapCheckpoints mapCheckpoints =
             boost::assign::map_list_of
-            (  1, uint256("0xcf2f78756f0fa64bc4ce80e6d500661cc4c20f2be28c7d859467dceb0adfa2de"))
-            ( 53, uint256("0x9343eae8db94d5d5e945b0c0a6b83647b6a3a8fd89cd170a757e06dcbf2e3bed"))
-            (117, uint256("0x5208d62f44467e800a92bfb18fc0fd4c39d9fed28f4ad160262f96dd90111ec3"))
-            (200, uint256("0xa5ce00c4aab4f9deccbef0af27adadb29cbf111eb442e92895d7302eb047ad4e"))
+            (     1, uint256("0xcf2f78756f0fa64bc4ce80e6d500661cc4c20f2be28c7d859467dceb0adfa2de"))
+            (    53, uint256("0x9343eae8db94d5d5e945b0c0a6b83647b6a3a8fd89cd170a757e06dcbf2e3bed"))
+            (   117, uint256("0x5208d62f44467e800a92bfb18fc0fd4c39d9fed28f4ad160262f96dd90111ec3"))
+            (   200, uint256("0xa5ce00c4aab4f9deccbef0af27adadb29cbf111eb442e92895d7302eb047ad4e"))
+            (  6452, uint256("0xe502fdfb3a35ee853ccd4a68433b1f9bbe3295c7d453fbcc484d06a766971475"))
+            ( 10978, uint256("0x88fcee5009a0febf7832750d0246bf4a9b88f8195befc795e5f34b0d1e0e92f9"))
+            ( 17954, uint256("0x7d40a9b80dd1b585b36e092aefcd3e579ef38f3180ea55dac53ead486f5d9cd2"))
+            ( 23978, uint256("0x6f111b6eef7dccc2da3c85014964aa402f39c684ba5709b576777503c87141af"))
+            ( 33212, uint256("0xe3b53359c1b088ec1f772d53eaa765d5c7410f0d9914e69bdb2a0fc881ddc9e8"))
 			;
         /*
         boost::assign::map_list_of
